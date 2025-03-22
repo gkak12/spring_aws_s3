2bb535b7446798e2579b4a11adc583fd9ea19c33
hyunjun <hyunjun5681@kakao.com>
2025-03-22 15:31:00 +0900

    try-catch 추가

--- src/main/java/com/spring/aws/s3/common/AwsS3Handler.java
+++ src/main/java/com/spring/aws/s3/common/AwsS3Handler.java
@@ -10,6 +10,8 @@ import org.springframework.stereotype.Component;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.WebIdentityTokenFileCredentialsProvider;
 import software.amazon.awssdk.core.ResponseInputStream;
+import software.amazon.awssdk.core.exception.SdkClientException;
+import software.amazon.awssdk.core.exception.SdkServiceException;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
@@ -17,6 +19,7 @@ import software.amazon.awssdk.services.s3.S3ClientBuilder;
 import software.amazon.awssdk.services.s3.model.*;
 
 import java.io.IOException;
+import java.nio.file.AccessDeniedException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -44,9 +47,9 @@ public class AwsS3Handler {
             }
 
             WebIdentityTokenFileCredentialsProvider credentialsProvider = WebIdentityTokenFileCredentialsProvider.builder()
-                    .roleArn(roleArn)
-                    .webIdentityTokenFile(tokenFilePath)
-                    .build();
+                .roleArn(roleArn)
+                .webIdentityTokenFile(tokenFilePath)
+                .build();
 
             builder.credentialsProvider(credentialsProvider);
         }
@@ -57,9 +60,9 @@ public class AwsS3Handler {
     public void putObject(AwsS3RequestDto awsS3RequestDto) {
         try {
             PutObjectRequest putObjectRequest = PutObjectRequest.builder()
-                    .bucket(awsS3RequestDto.getBucketName())
-                    .key(awsS3RequestDto.getFileName())
-                    .build();
+                .bucket(awsS3RequestDto.getBucketName())
+                .key(awsS3RequestDto.getFileName())
+                .build();
 
             PutObjectResponse response = this.s3Client.putObject(
                 putObjectRequest
@@ -74,30 +77,65 @@ public class AwsS3Handler {
                 log.error("ERROR: AWS S3 upload object to S3.");
                 throw new RuntimeException("ERROR: AWS S3 upload object to S3.");
             }
+        } catch (AccessDeniedException e) {
+            log.error("ERROR: AccessDeniedException.");
+            throw new RuntimeException("ERROR: AccessDeniedException.", e);
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            log.error("ERROR: IOException.");
+            throw new RuntimeException("ERROR: IOException.", e);
+        } catch (NoSuchBucketException e) {
+            log.error("ERROR: NoSuchBucketException.");
+            throw new RuntimeException("ERROR: NoSuchBucketException.", e);
+        } catch (S3Exception e) {
+            log.error("ERROR: S3Exception.");
+            throw new RuntimeException("ERROR: S3Exception.", e);
+        } catch (SdkServiceException e) {
+            log.error("ERROR: SdkServiceException.");
+            throw new RuntimeException("ERROR: SdkServiceException.", e);
+        } catch (SdkClientException e) {
+            log.error("ERROR: SdkClientException.");
+            throw new RuntimeException("ERROR: SdkClientException.", e);
+        } catch (Exception e) {
+            log.error("ERROR: Exception.");
+            throw new RuntimeException("ERROR: Exception.", e);
         }
     }
 
     public Resource getObject(AwsS3RequestDto awsS3RequestDto) {
-        GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+        try {
+            GetObjectRequest getObjectRequest = GetObjectRequest.builder()
                 .bucket(awsS3RequestDto.getBucketName())
                 .key(awsS3RequestDto.getFileName())
                 .build();
 
-        ResponseInputStream<GetObjectResponse> s3ObjectStream = s3Client.getObject(getObjectRequest);
-        return new InputStreamResource(s3ObjectStream);
+            ResponseInputStream<GetObjectResponse> s3ObjectStream = s3Client.getObject(getObjectRequest);
+            return new InputStreamResource(s3ObjectStream);
+        } catch (NoSuchKeyException e) {
+            throw new RuntimeException("ERROR: 요청한 파일이 존재하지 않습니다.", e);
+        } catch (SdkServiceException e) {
+            throw new RuntimeException("ERROR: AWS 서비스 오류 발생했습니다.", e);
+        } catch (Exception e) {
+            log.error("ERROR: Exception");
+            throw new RuntimeException("ERROR: Exception", e);
+        }
     }
 
     public boolean deleteObject(AwsS3RequestDto awsS3RequestDto) {
-        DeleteObjectRequest deleteObjectRequest = DeleteObjectRequest.builder()
+        try {
+            DeleteObjectRequest deleteObjectRequest = DeleteObjectRequest.builder()
                 .bucket(awsS3RequestDto.getBucketName())
                 .key(awsS3RequestDto.getFileName())
                 .build();
 
-        DeleteObjectResponse response = s3Client.deleteObject(deleteObjectRequest);
-
-        boolean res = response.sdkHttpResponse().statusCode() == 204 ? true : false;
-        return res;
+            DeleteObjectResponse response = s3Client.deleteObject(deleteObjectRequest);
+            return response.sdkHttpResponse().statusCode() == 204 ? true : false;
+        } catch (NoSuchKeyException e) {
+            throw new RuntimeException("ERROR: 삭제하려는 파일이 존재하지 않습니다.", e);
+        } catch (SdkServiceException e) {
+            throw new RuntimeException("ERROR: AWS 서비스 오류 발생했습니다.", e);
+        } catch (Exception e) {
+            log.error("ERROR: Exception");
+            throw new RuntimeException("ERROR: Exception", e);
+        }
     }
 }
