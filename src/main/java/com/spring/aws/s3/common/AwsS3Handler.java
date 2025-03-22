6240b0571a9d6e5e3fab8702aa3b341a033ccc96
hyunjun <hyunjun5681@kakao.com>
2025-03-22 14:08:43 +0900

    AWS S3 업로드 기능 추가

--- src/main/java/com/spring/aws/s3/common/AwsS3Handler.java
+++ src/main/java/com/spring/aws/s3/common/AwsS3Handler.java
@@ -4,12 +4,18 @@ import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.WebIdentityTokenFileCredentialsProvider;
+import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.S3ClientBuilder;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectResponse;
 
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -45,5 +51,25 @@ public class AwsS3Handler {
         }
 
         this.s3Client = builder.build();
+    }
+
+    public void putObject(String bucket, String key, MultipartFile mfile) {
+        try {
+            PutObjectResponse response = this.s3Client.putObject(
+                PutObjectRequest.builder().bucket(bucket).key(key).build()
+                , RequestBody.fromBytes(mfile.getBytes())
+            );
+
+            int statusCode = response.sdkHttpResponse().statusCode();
+
+            if(statusCode == 200 || statusCode == 204){
+                log.info("SUCCESS: AWS S3 upload object to S3.");
+            } else {
+                log.error("ERROR: AWS S3 upload object to S3.");
+                throw new RuntimeException("ERROR: AWS S3 upload object to S3.");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
